fe0ec54c0d9e7ac505bb6ace365fe032c001473d
Toby Jefferies <toby_jefferies@hotmail.com>
2024-03-05 16:34:39 +0000

    added about app file

--- about/views.py
+++ about/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
-
 # Create your views here.
-def index(request):
-    return HttpResponse("The Pizza Project")
-
-def 
+def about_us(request):
+    return HttpResponse("About Us")
--- pizza_project/urls.py
+++ pizza_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from site_build import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', index_views.index, name='index'),
+    path(''), about_views.about_us, name='about'
 ]
